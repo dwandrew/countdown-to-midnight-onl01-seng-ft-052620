645fa58294f6679ac555aaedf57a76398af0ca82
Daniel Andrew <danwandrew@gmail.com>
2020-05-26 16:41:09 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -3,8 +3,18 @@
 def countdown(num)
   while num>=1
   puts "#{num} SECOND(S)!"
-  
   num-=1;
   end
   "HAPPY NEW YEAR!"
 end
+
+def countdown_with_sleep(num)
+  while num>=1
+  puts "#{num} SECOND(S)!"
+  sleep(1)
+  num-=1;
+  end
+  "HAPPY NEW YEAR!"
+end
+
+  
